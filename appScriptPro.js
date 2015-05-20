a8c29b60a6e781e16f30747769fcddb255a7bc8e
Jack <jptookey@yahoo.com>
2015-05-19 20:08:51 -0400

    More Testing

--- appScriptPro.js
+++ appScriptPro.js
@@ -49,36 +49,23 @@ function enableC2() {
         // dialogForce: true,
         blankContent: "<div class='dialogCont'>" +
         "<div class='text1 diagHeader color1' id='diagHeadLarge'><span class='middle'>" +
-        "Please enter the following information to create an account. </span></div>" +
-        "<input class='border1' id='userName' placeholder='User Name'/input>" +
-        "<input class='border1' id='firstName' placeholder='First Name'/input>" +
-        "<input class='border1' id='lastName' placeholder='Last Name'/input>" +
-        "<table id='genderT'><tr><td><label class='gender' id='labelM'>Male</label></td><td><label class='gender' id='labelF'>Female</label></td></tr>" +
-        "<tr><td><input class='gender' type='radio' name='gender' id='genderM' value='M'/input></td><td>"+
-        "<input class='gender' type='radio' name='gender' id='genderF' value='F'/input></td></tr></table>"+
-        "<input class='border1' id='zipCode' placeholder='Zip Code'/input>" +
-        "<p id='zipValidate'></p>"+
-        "<input class='border1' id='DOB' type='date' placeholder='Date of Birth'/input>" +
-        "<input class='border1' id='emailNew' placeholder='Email Address'/input>" +
-        "<p id='emailValidate'></p>"+
-        "<input class='border1' id='pwNew1' type='password' placeholder='Password'/input>" +
-        "<input class='border1' id='pwNew2' type='password' placeholder='Repeat Password'/input>" +
+        "Please enter the following to create a new password. </span></div>" +
+        "<input class='border1' id='pwOld' type='password' placeholder='Old Password'/input>" + "</br>"+
+            "<p id=pwVal></p>"+
+        "<input class='border1' id='pwNew1' type='password' placeholder='New Password'/input>" +
+        "<input class='border1' id='pwNew2' type='password' placeholder='Repeat New Password'/input>" +
         "<p id='pwCompare'></p>"+
-        "<div><a onclick='login()' data-role='button' class='border1 text1 background1' id='backButton1' rel='close'></a>" +
-        "<a onclick='submitUser1()' data-role='button' class='border1 text1 background1' id='nextButton1'></a></div>" +
+        "<div><a data-role='button' class='border1 text1 background1' id='backButton1' rel='close'></a>" +
+        "<a onclick='submitPW()' data-role='button' class='border1 text1 background1' id='nextButton1'></a></div>" +
         "</div>"
     });
-    $('#emailNew').focusout(function(){
-        email = document.getElementById('emailNew').value;
-        if(validateEmail(email)) {
-            var emailVal3 = {
-                emailVal: email
-            };
-            var emailVal2 = JSON.stringify(emailVal3);
-            console.log(emailVal2);
+    $('#pwOld').focusout(function(){
+        var pwvar = document.getElementById('pwOld').value;
+        var emailVal2 = JSON.stringify(pwvar);
+        //console.log(emailVal2);
             $.ajax({
                 type: "POST",
-                url: URLout +"scripts/emailVal.php",
+                url: "/scripts/checkPW.php",
                 data: emailVal2,
                 cache: false,
                 dataType: "json",
@@ -86,10 +73,10 @@ function enableC2() {
                 {
                     console.log(data);
                     if (data > 0) {
-                        $("#emailValidate").show().text('Email already in use');
-                        $("#emailNew").val('');
+                        $("#pwVal").show().text('Invalid Password');
+                        $("#pwOld").val('');
                     } else {
-                        $("#emailValidate").hide().text('Email already in use');
+                        $("#pwVal").hide();
                     }
 
                 },
@@ -99,9 +86,7 @@ function enableC2() {
                     console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
                 }
             });
-        } else {
-            $('#emailValidate').show().text('Please enter a valid email');
-        }
+
     });
 }
 
@@ -109,6 +94,33 @@ function validateEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
 }
+
+function submitPW() {
+    var pw1 = document.getElementById('pwNew1').value;
+    var pw2 = document.getElementById('pwNew2').value;
+    if (pw1===pw2) {
+        var pw3 = json.stringify(pw1);
+        $.ajax({
+            type: "POST",
+            url: "/scripts/pushPW.php",
+            data: emailVal2,
+            cache: false,
+            dataType: "json",
+            success: function(data)
+            {
+               return('done');
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log( jqXHR.responseText);
+                console.log(JSON.stringify(jqXHR));
+                console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
+            }
+        });
+    } else {
+        $("#pwVal2").show().text('Passwords do not match');
+    }
+}
+
 
 //TODO: Add a dialog for changing password
 //TODO: Make EDIT button enable everything for changes
