4b03881c4dbaa351842c2f61d7a1ba5a8c903602
Jack <jptookey@yahoo.com>
2015-06-25 19:30:54 -0400

    Beginning Work on the Notifications Page

--- appScriptNot.js
+++ appScriptNot.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
     console.log(localStorage.userKey);
     // var email = localStorage.email;
     if (localStorage.getItem("userKey") === null) {
-        window.location.assign("http://ibtee.com");
+        window.location.assign("/Ponghole.html");
     } else {
 
         userKey = localStorage.userKey;
@@ -12,3 +12,24 @@ $( document ).ready(function() {
        // loadstats();
     }
 });
+
+function goback() {
+    window.location.assign("/Ponghole.html");
+}
+
+
+//TODO: Add an append function that uses HTML and a function with an 'i' for a for-loop.
+
+/*
+
+AJAX into ARRAY
+
+var lengthi = ARRAY.length
+for(i=0;i<lengthi;i++) {
+$("BigBox").append("<div class='bubble' onclick='function("+i+")>Gamestatsblablabla</div>");
+
+function(i) {
+    SimpleDialog box with ARRAY[i] information
+    }
+
+ */
