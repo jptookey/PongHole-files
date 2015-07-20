ea198a4a927cc41a89765f603035e5107345e4e0
Jack <jptookey@yahoo.com>
2015-07-20 19:29:10 -0400

    Beginning Work on the Notifications Page

--- appScriptNot.js
+++ appScriptNot.js
@@ -53,15 +53,15 @@ function buildBubs() {
             "<div class='n_form_1'>Opponent 1: "+notes1[i][5]+"</div>" +
             "<div class='n_form_1'>Teammate: "+notes1[i][6]+" </div>" +
             "<div class='n_form_1'>Opponent 2: "+notes1[i][7]+"</div>" +
-            "<div class='n_form_1' id='n_win'>Score: "+notes1[i][9]+"-"+notes1[i][10]+" "+notes1[i][2]+"</div>" +
+            "<div class='n_form_1 border1' id='n_win'>Score: "+notes1[i][9]+"-"+notes1[i][10]+" "+notes1[i][2]+"</div>" +
             "</div>"
         );
         console.log(notes1[i][2] == 'L')
         if(notes1[i][2] == 'W') {
-            $('n_win').addClass("n_form_win");
+            $('#n_win').addClass("n_form_win");
         }
         else if (notes1[i][2] == 'L'){
-            $('n_win').addClass("n_form_lose");
+            $('#n_win').addClass("n_form_lose");
         }
 
     }
